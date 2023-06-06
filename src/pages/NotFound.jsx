fc2f6f2f0460df62394936569ea419208ece444d
Ardy Pranata <ardiprana90@gmail.com>
2023-06-07 06:55:01 +0700

    commits

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <>
-      <main className="min-h-full relative w-full flex h-screen content-center items-center justify-center place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="min-h-full relative w-full flex h-screen content-center items-center justify-center place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <p className="text-center text-4xl font-bold tracking-tight text-white sm:text-6xl">
             404
           </p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
             Page not found
           </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
+          <p className="mt-6 text-base leading-7 text-gray-200">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 import withMT from "@material-tailwind/react/utils/withMT";
  
-export default withMT({
+module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
